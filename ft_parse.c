118ee7948f848496eca0e39a35b6773f247c94be
martin <martin.marek@email.cz>
2024-06-20 10:31:57 +0200

    add parse and pipe

--- ft_parse.c
+++ ft_parse.c
@@ -6,30 +6,91 @@
 /*   By: mmarek <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 20:25:45 by mmarek            #+#    #+#             */
-/*   Updated: 2024/06/15 09:41:21 by mmarek           ###   ########.fr       */
+/*   Updated: 2024/06/18 23:59:37 by mmarek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_cmd	ft_parse(char *line)
+
+
+void	ft_parse(char *line, char pars, char *arr[], t_cmd *command)
 {
-	t_cmd		command;
-	int			count;
-	char		*delimiter = " ";
-	char		*token;
+	char 	*s;
+	char 	*pars_line;
+	int		i;
 
-	count = 0;
+	//printf("%p  %p \n",  arr, command);
+	//*arr = (char *)calloc(32 , sizeof(char));
+	pars_line = (char *)calloc(32 , sizeof(char));
 
-	token = strtok(line, delimiter);
-	while (token != NULL)
+	s = line;
+	i = 0;
+	while (*s == ' ')
+		s++;
+	while (*s != pars && *s != '\0')
 	{
-		command.cmd[count] = strdup(token);
-		token = strtok(NULL, delimiter);
-		count++;
+		//if (*s != ' ')
+		//{
+			*(pars_line + i)= *s;
+			i++;
+		//}
+		s++;
 	}
-	command.cmd[count] = token;
-	command.type = 1;
+	*(*arr + i) = '\0';
+	while (*s == ' ')
+		s++;
+	parse_arr(pars_line, ' ', arr);
+	if (*s != '\0')
+	{
+		command->next = (t_cmd *)malloc(sizeof(t_cmd));
+		command->next->prev = command;
+		command = command->next;
+		*command->cmd = (char *)calloc(10 , sizeof(char));
+		command->next = NULL;
+		command->type = 0;
+		//if(*s == pars)
+		ft_parse(s + 1, pars, command->cmd, command);
+		//else
+		//	ft_parse(s, pars, arr + 1, command);
+	}
 
-	return (command);
+	free(pars_line);
+	//else
+		//*(arr + 1) = NULL;
+}
+
+void	parse_arr(char *line, char pars, char *arr[])
+{
+	char 	*s;
+	int		i;
+
+	//printf("%p  %p \n",  arr, command);
+	*arr = (char *)calloc(32 , sizeof(char));
+
+	s = line;
+	i = 0;
+	while (*s == ' ')
+		s++;
+	while (*s != pars && *s != '\0')
+	{
+		//if (*s != ' ')
+		//{
+			*(*arr + i)= *s;
+			i++;
+		//}
+		s++;
+	}
+	*(*arr + i) = '\0';
+	while (*s == ' ')
+		s++;
+	if (*s != '\0')
+	{
+		if(*s == pars)
+			parse_arr(s + 1, pars, arr + 1);
+		else
+			parse_arr(s, pars, arr + 1);
+	}
+	else
+		*(arr + 1) = NULL;
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: mmarek <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 20:27:15 by mmarek            #+#    #+#             */
-/*   Updated: 2024/06/13 16:17:49 by mmarek           ###   ########.fr       */
+/*   Updated: 2024/06/19 23:17:53 by mmarek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,19 @@
 
 typedef struct s_cmd
 {
-	char	*cmd[1024];
-	int		type;
+	int				type;
+	char			*cmd[10];
+	struct s_cmd	*next;
+	struct s_cmd	*prev;
 }	t_cmd;
+
+typedef struct s_list
+{
+	int				type;
+	char			*cmd[10];
+	struct s_list	*next;
+	struct s_cmd	*prev;
+}	t_list;
 
 //typedef struct s_redir
 //{
@@ -56,7 +66,14 @@ typedef struct s_cmd
 
 
 
-t_cmd	ft_parse(char *line);
-void	ft_execute(t_cmd command);
+void	ft_parse(char *line, char pars, char *arr[], t_cmd *command);
+void	parse_arr(char *line, char pars, char *arr[]);
+void	ft_execute(t_cmd *command);
+void	call_exec(t_cmd *list);
+void	call_pipe(t_cmd *list, int fdd[2]);
+void	input_pipe(t_cmd *list);
+void	output_pipe(t_cmd *list, int fdd[2]);
+void	middle_pipe(t_cmd *list, int fd[2]);
+void	free_command(t_cmd *cmd);
 
 #endif
